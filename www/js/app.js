3b9a308baa31d6c22e6dd1c55391ce5e0e400b7a
vismay-golwala <golwalavismay9@gmail.com>
2016-12-13 20:04:30 +0530

    type resolve

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,42 @@
 var nameApp = angular.module('starter', ['ionic']);
 
+nameApp.run(function($rootScope) {
+    $rootScope.coffee = [{name: 'Cappuccino', imgFile:'cappuccinoCoffee.jpg', price: 100},  
+                {name: 'Cortado',imgFile:'cortadoCoffee.jpg', price: 50},
+                {name: 'Con panna',imgFile:'conPannaCoffee.jpg', price: 100},
+                {name: 'Corretto',imgFile:'correttoCoffee.jpg',price: 100},
+                {name: 'Doppio',imgFile:'doppioCoffee.jpg', price: 100},
+                {name: 'Espresso Lungo',imgFile:'espressoLungoCoffee.jpg', price: 100},
+                {name: 'Espresso Romano',imgFile:'espressoRomanoCoffee.jpg', price: 100},
+                {name: 'Long Black',imgFile:'longBlackCoffee.jpg', price: 100},
+                {name: 'Macchiato',imgFile:'macchiatoCoffee.jpg', price: 100},
+                {name: 'Ristretto', imgFile:'ristrettoCoffee.jpg',price: 100}];
+
+    $rootScope.icecream = [{name: 'Strawberry',imgFile:'strawberryIce.jpg', price: 100},  
+                          {name: 'Chocolate',imgFile:'chocolateIce.jpg',price: 50},
+                          {name: 'Kiwi',imgFile:'kiwiIce.jpg', price: 100},
+                          {name: 'Gauva', imgFile:'gauvaIce.jpg',price: 100},
+                          {name: 'Pineaple',imgFile:'pineapleIce.jpg', price: 100},
+                          {name: 'Sitafal',imgFile:'sitafalIce.jpg',price: 100},
+                          {name: 'Black Current',imgFile:'blackCurrentIce.jpg', price: 100},
+                          {name: 'Cookie Cream',imgFile:'cookieCreamIce.jpg', price: 100},
+                          {name: 'Almond Carnival',imgFile:'almondCarnivalIce.jpg', price: 100},
+                          {name: 'American Nuts',imgFile:'americanNutsIce.jpg', price: 100},
+                          {name: 'Chocolate Chips',imgFile:'chocolateChipsIce.jpg', price: 100}];
+
+    $rootScope.juice = [{name: 'Orange',imgFile:'orangeJuice.jpg', price: 100},  
+                        {name: 'Strawberry',imgFile:'strawberryJuice.jpg', price: 50},
+                        {name: 'Kiwi',imgFile:'kiwiJuice.jpg', price: 100},
+                        {name: 'Pineaple',imgFile:'pineapleJuice.jpg', price: 100},
+                        {name: 'Mango',imgFile:'mangoJuice.jpg',price: 100},
+                        {name: 'Grapes',imgFile:'grapesJuice.jpg', price: 100},
+                        {name: 'Chikoo',imgFile:'chikooJuice.jpg', price: 100},
+                        {name: 'Litchi',imgFile:'litchiJuice.jpg', price: 100},
+                        {name: 'Water Melon',imgFile:'watermelonJuice.jpg', price: 100},
+                        {name: 'Kiwi',imgFile:'kiwiJuice.jpg', price: 100},
+                        {name: 'Mix Fruit',imgFile:'mixFruitJuice.jpg',price:100}];
+});
+
 nameApp.config(function($stateProvider, $urlRouterProvider) {
  
   $stateProvider
@@ -43,47 +80,18 @@ nameApp.config(function($stateProvider, $urlRouterProvider) {
  
 });
 
- nameApp.controller('cofeeCtrl',function($scope) {
-   $scope. coffeeTypes = [{name: 'Cappuccino', imgFile:'cappuccinoCoffee.jpg', price: 100},  
-                          {name: 'Cortado',imgFile:'cortadoCoffee.jpg', price: 50},
-                          {name: 'Con panna',imgFile:'conPannaCoffee.jpg', price: 100},
-                          {name: 'Corretto',imgFile:'correttoCoffee.jpg',price: 100},
-                          {name: 'Doppio',imgFile:'doppioCoffee.jpg', price: 100},
-                          {name: 'Espresso Lungo',imgFile:'espressoLungoCoffee.jpg', price: 100},
-                          {name: 'Espresso Romano',imgFile:'espressoRomanoCoffee.jpg', price: 100},
-                          {name: 'Long Black',imgFile:'longBlackCoffee.jpg', price: 100},
-                          {name: 'Macchiato',imgFile:'macchiatoCoffee.jpg', price: 100},
-                          {name: 'Ristretto', imgFile:'ristrettoCoffee.jpg',price: 100}];
+ nameApp.controller('cofeeCtrl',function($scope, $rootScope) {
+   $scope. coffeeTypes = $rootScope.coffee;
                     
   });
 
-  nameApp.controller('IceCreamCtrl',function($scope) {
-   $scope. iceCreamTypes =[{name: 'Strawberry',imgFile:'strawberryIce.jpg', price: 100},  
-                          {name: 'Chocolate',imgFile:'chocolateIce.jpg',price: 50},
-                          {name: 'Kiwi',imgFile:'kiwiIce.jpg', price: 100},
-                          {name: 'Gauva', imgFile:'gauvaIce.jpg',price: 100},
-                          {name: 'Pineaple',imgFile:'pineapleIce.jpg', price: 100},
-                          {name: 'Sitafal',imgFile:'sitafalIce.jpg',price: 100},
-                          {name: 'Black Current',imgFile:'blackCurrentIce.jpg', price: 100},
-                          {name: 'Cookie Cream',imgFile:'cookieCreamIce.jpg', price: 100},
-                          {name: 'Almond Carnival',imgFile:'almondCarnivalIce.jpg', price: 100},
-                          {name: 'American Nuts',imgFile:'americanNutsIce.jpg', price: 100},
-                          {name: 'Chocolate Chips',imgFile:'chocolateChipsIce.jpg', price: 100}];
+  nameApp.controller('IceCreamCtrl',function($scope, $rootScope) {
+   $scope. iceCreamTypes =$rootScope.icecream;
                     
   });
 
-  nameApp.controller('JuicesCtrl',function($scope) {
-   $scope. juiceTypes =[{name: 'Orange',imgFile:'orangeJuice.jpg', price: 100},  
-                          {name: 'Strawberry',imgFile:'strawberryJuice.jpg', price: 50},
-                          {name: 'Kiwi',imgFile:'kiwiJuice.jpg', price: 100},
-                          {name: 'Pineaple',imgFile:'pineapleJuice.jpg', price: 100},
-                          {name: 'Mango',imgFile:'mangoJuice.jpg',price: 100},
-                          {name: 'Grapes',imgFile:'grapesJuice.jpg', price: 100},
-                          {name: 'Chikoo',imgFile:'chikooJuice.jpg', price: 100},
-                          {name: 'Litchi',imgFile:'litchiJuice.jpg', price: 100},
-                          {name: 'Water Melon',imgFile:'watermelonJuice.jpg', price: 100},
-                          {name: 'Kiwi',imgFile:'kiwiJuice.jpg', price: 100},
-                          {name: 'Mix Fruit',imgFile:'mixFruitJuice.jpg',price:100}];
+  nameApp.controller('JuicesCtrl',function($scope, $rootScope) {
+   $scope. juiceTypes = $rootScope.juice;
                     
   });
 
@@ -135,15 +143,17 @@ nameApp.controller('JuiceCtrl', function($scope,  Authorization) {
   $scope.input = Authorization;  
 });
 
-nameApp.controller('CoffeeDetailsCtrl', function($scope,  Authorization) {
-  $scope.input = Authorization;  
+nameApp.controller('CoffeeDetailsCtrl', function($scope,  Authorization, $stateParams, $filter, $rootScope) {
+  $scope.input = Authorization;
+  $scope.type = $filter('filter')($rootScope.coffee, {name: $stateParams.coffeename})[0];
 });
 
-nameApp.controller('IceCreamDetailsCtrl', function($scope,  Authorization) {
-  $scope.input = Authorization;  
+nameApp.controller('IceCreamDetailsCtrl', function($scope,  Authorization, $stateParams, $filter, $rootScope) {
+  $scope.input = Authorization;
+  $scope.type = $filter('filter')($rootScope.icecream, {name: $stateParams.iceCreamName})[0];  
 });
 
-nameApp.controller('JuiceDetailsCtrl', function($scope,  Authorization) {
+nameApp.controller('JuiceDetailsCtrl', function($scope,  Authorization, $stateParams, $filter, $rootScope) {
   $scope.input = Authorization;  
+  $scope.type = $filter('filter')($rootScope.juice, {name: $stateParams.juiceName})[0];
 });
-
